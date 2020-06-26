81b11a53c7e04f97180c6fd036813b8fc2f78bf6
Marco Westerhof <m.westerhof@lukkien.com>
2020-06-26 15:58:26 +0200

    foo

--- hooks/testhook.py
+++ hooks/testhook.py
@@ -1,9 +1,15 @@
-from utils import only_on_branch
+from utils import only_for_branch, skip_for_branch
 
 
-@only_on_branch('master')
+@only_for_branch('master')
 def test_hook():
-    print("running test_hook")
+    print("running test_hook(runs first, only for master)")
+
+
+@skip_for_branch('wip', r'wip\/.*')
+def test_hook_2():
+    print("running test_hook_2(runs second, not for wip)")
 
 
 test_hook()
+test_hook_2()
